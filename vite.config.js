bce38efad5568fe7a2e9d95eef9fc6d89969a4c5
TomuCard <tomu.cardonnel@gmail.com>
2025-03-27 19:12:04 +0100

    jsp

--- vite.config.js
+++ vite.config.js
@@ -1,44 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'; 
+import path from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
+  plugins: [react()],
   resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src')
-      },
-      {
-        find: '@layout',
-        replacement: path.resolve(__dirname, 'src/components/layout')
-      },
-      {
-        find: '@partials',
-        replacement: path.resolve(__dirname, 'src/components/partials')
-      },
-      {
-        find: '@css',
-        replacement: path.resolve(__dirname, 'src/css')
-      },
-      {
-        find: '@assets',
-        replacement: path.resolve(__dirname, 'src/assets')
-      },
-      {
-        find: '@screen',
-        replacement: path.resolve(__dirname, 'src/assets/screen')
-      },
-      {
-        find: '@icons',
-        replacement: path.resolve(__dirname, 'src/assets/icons')
-      },
-    ]
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+      '@layout': path.resolve(__dirname, 'src/components/layout'),
+      '@partials': path.resolve(__dirname, 'src/components/partials'),
+      '@css': path.resolve(__dirname, 'src/css'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
+      '@screen': path.resolve(__dirname, 'src/assets/screen'),
+      '@icons': path.resolve(__dirname, 'src/assets/icons')
+    }
   }
 })
